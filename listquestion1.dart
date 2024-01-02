95ab686fd5d6c698b8f3224794f5c28603520d97
Hansraj kumar <hansrajtechofficial@gmail.com>
2024-01-02 17:02:55 +0530

    updated data in new list question

--- listquestion1.dart
+++ listquestion1.dart
@@ -1,45 +1,30 @@
-// void main() {
-//   List<int> number = [
-//     1,
-//     2,
-//     1,
-//     2,
-//     12,
-//     2,
-//     5,
-//     1,
-//     5,
-//     4,
-//     8,
-//   ];
-//   List<int> result = [];
-
-//   for (int i = 0; i < number.length; i++) {
-//     bool found = false;
-//     for (int j = 0; j < result.length; j++) {
-//       if (j < 0) {
-//         result.add(number[i]);
-//       }
-//       if (number[i] == result[j]) {
-//         found = true;
-//         break;
-//       }
-//     }
-//     if (!found) {
-//       result.add(number[i]);
-//     }
-//   }
-//   print(result);
-// }
 void main() {
-  List<int> number = [1, 2, 1, 2, 12, 2, 5, 1, 5, 4, 8];
+  List<int> number = [0, 0, 0, 0, 1, 2, 2, 2, 1, 1, 1, 2, 2, 2, 1, 1, 1];
   List<int> result = [];
+
   for (int i = 0; i < number.length; i++) {
-    if (result.contains(number[i])) {
-      continue;
-    } else {
+    bool found = false;
+    for (int j = 0; j < result.length; j++) {
+      if (number[i] == result[j]) {
+        found = true;
+        break;
+      }
+    }
+    if (!found) {
       result.add(number[i]);
     }
   }
   print(result);
 }
+// void main() {
+//   List<int> number = [1, 2, 1, 2, 12, 2, 5, 1, 5, 4, 8];
+//   List<int> result = [];
+//   for (int i = 0; i < number.length; i++) {
+//     if (result.contains(number[i])) {
+//       continue;
+//     } else {
+//       result.add(number[i]);
+//     }
+//   }
+//   print(result);
+// }
