af30c70cb5050617c7995c203b971433b675b6ae
Hansraj kumar <hansrajtechofficial@gmail.com>
2024-01-05 18:34:50 +0530

    basic approach in new quesiton

--- samenuminlist.dart
+++ samenuminlist.dart
@@ -1,5 +1,5 @@
-//question
-//a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+// question
+// a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 //  b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 // and write a program that returns a list that contains only the
 // elements that are common between them (without duplicates).
@@ -7,28 +7,7 @@
 
 void main() {
   List<int> a = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
-  List<int> b = [
-    1,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10,
-    11,
-    12,
-    13
-  ];
+  List<int> b = [1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13];
   List<int> result = [];
 
   for (int i = 0; i < a.length; i++) {
@@ -40,7 +19,12 @@ void main() {
       }
     }
     if (!isDuplicate) {
-      result.add(a[i]);
+      for (int k = 0; k < b.length; k++) {
+        if (a[i] == b[k]) {
+          result.add(a[i]);
+          break; // Break once a match is found in list b
+        }
+      }
     }
   }
   print(result);
