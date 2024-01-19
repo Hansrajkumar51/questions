bb7a96505836a32459c77fa5b604eb4f0f4f6dde
Hansraj kumar <hansrajtechofficial@gmail.com>
2024-01-19 18:46:20 +0530

    hard question added

--- hard.dart
+++ hard.dart
@@ -1,18 +1,35 @@
-import 'dart:convert';
-
 void main() {
   List<List<int>> num = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9],
   ];
-  List<int> result = [];
 
-  for (int i = 0; i < num[0].length; i++) {
-    for (int j = 0; j < num.length; j++) {
-      result.add(num[i][j]);
+  List<List<int>> result = [];
+// Generating combinations of pairs (2 elements)
+  for (var i = 0; i < num.length; i++) {
+    for (var j = 0; j < num[i].length; j++) {
+      for (var k = 0; k < num.length; k++) {
+        if (k != i) {
+          // Ensure elements are from different rows
+          for (var l = 0; l < num[k].length; l++) {
+            result.add([num[i][j], num[k][l]]);
+          }
+        }
+      }
     }
   }
+
+  // Generating combinations of triples (3 elements)
+  // for (var i = 0; i < num.length; i++) {
+  //   for (var j = 0; j < num[i].length; j++) {
+  //     for (var k = 0; k < num[i].length; k++) {
+  //       for (var l = 0; l < num[i].length; l++) {
+  //         result.add([num[i][j], num[i][k], num[i][l]]);
+  //       }
+  //     }
+  //   }
+  // }
 
   print(result);
 }
